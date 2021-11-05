5fdc1ea46e9b6e6c60af98df58e40fa1b9b684de
Documenter.jl <documenter@juliadocs.github.io>
2021-11-05 17:23:16 +0000

    build based on 7228f56

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SunAsAStar","category":"page"},{"location":"#SunAsAStar","page":"Home","title":"SunAsAStar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SunAsAStar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SunAsAStar]","category":"page"}]
+}
