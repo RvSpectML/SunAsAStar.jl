1970702612fa991d680a249173cc7d3816af6dfb
Eric Ford <ebf11@psu.edu>
2021-11-08 19:39:09 -0500

    tweak mkdirs

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,6 @@
-if !isdir("data") mkdir("data") end
-if !isdir("data/horizons") mkdir("data/horizons") end
+if !isdir("../data") mkdir("../data") end
+if !isdir("../data/horizons") mkdir("../data/horizons") end
+println("build.jl running from ", pwd())
 
 #include("build_julia_specific_conda.jl")
 include("pyimports.jl")
